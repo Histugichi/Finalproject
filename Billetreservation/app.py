99da3e90a7c0d30dc4f64abfa062a4e07b384a92
Histugichi <163933432+Histugichi@users.noreply.github.com>
2024-04-12 19:08:52 -0400

    test

--- Billetreservation/app.py
+++ Billetreservation/app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, session, url_for, redirect
 
 
 app = Flask(__name__)
+app.secret_key='secretkey'
 
 @app.route('/')
-def accueil():
-    return render_template('accueil.html')
+def home():
+    return render_template('home.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/service')
 def service():
     return render_template('service.html')
+
 
 @app.route('/evenement')
 def evenement():
     return render_template('evenement.html')
 
-@app.route('/reservation')
-def reservation():
-    return render_template('reservation.html')
+
+@app.route('/paiement')
+def paiement():
+    return render_template('paiement.html')
