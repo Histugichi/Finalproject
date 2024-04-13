b641ff5855a65638d73d1418eb9d814c2c0ca8c2
Histugichi <163933432+Histugichi@users.noreply.github.com>
2024-04-12 20:04:37 -0400

    recap

--- Billetreservation/app.py
+++ Billetreservation/app.py
@@ -1,29 +1,27 @@
-from flask import Flask, render_template, request, session, url_for, redirect
+from flask import Flask, render_template
 
-
-app = Flask(__name__)
-app.secret_key='secretkey'
+app = Flask(__name__, static_folder='C:/Users/maxle/OneDrive/Bureau/python/tp/ProjetFinal/static')
+app.secret_key = 'secretkey'
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
 @app.route('/login')
 def login():
     return render_template('login.html')
-
 
 @app.route('/service')
 def service():
     return render_template('service.html')
 
-
 @app.route('/evenement')
 def evenement():
     return render_template('evenement.html')
 
-
 @app.route('/paiement')
 def paiement():
     return render_template('paiement.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
