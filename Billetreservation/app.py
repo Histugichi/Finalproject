ba28ebdee49b18a4a79a22f839382cf0115037e8
Histugichi <163933432+Histugichi@users.noreply.github.com>
2024-04-14 19:59:00 -0400

    new

--- Billetreservation/app.py
+++ Billetreservation/app.py
@@ -13,37 +13,36 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route('/service')
-def service():
-    return render_template('service.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 @app.route('/evenement')
 def evenement():
     return render_template('evenement.html')
 
-@app.route('/reservations', methods=['POST'])
+@app.route('/reservations', methods=['POST', 'GET'])
 def reservations():
+    nom=None
+    email=None
+    date=None
     if request.method == 'POST':
-        # Récupérer les données du formulaire
         nom = request.form['nom']
         email = request.form['email']
         date = request.form['date']
-        # Ici, vous pouvez traiter les données de réservation, comme les enregistrer dans une base de données, etc.
-        return render_template('confirmation.html', nom=nom, email=email, date=date)
+    return render_template('reservations.html', nom=nom, email=email, date=date)
 
-@app.route('/')
+@app.route('/confirmation')
+def confirmation():
+    return render_template('confirmation.html')
+
+@app.route('/paiement', methods=['POST','GET'])
 def paiement():
-    return render_template('paiement.html')
-
-@app.route('/process_paiement', methods=['POST'])
-def process_paiement():
     if request.method == 'POST':
         card_number = request.form['card_number']
         expiry_date = request.form['expiry_date']
         cvv = request.form['cvv']
-        # Traiter les données du formulaire de paiement ici
-        # Par exemple, enregistrer les détails de paiement dans une base de données ou les transmettre à un service de paiement tiers
-        return "Paiement reçu avec succès!"
+    return render_template('paiement.html')
     
 
     
